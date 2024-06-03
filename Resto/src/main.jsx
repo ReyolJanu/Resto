a89c33dbaee5263ad429af15361e4997f24d1835
janu <ignatious.janukshan20@gmail.com>
2024-06-03 23:45:21 +0530

    2nd commit

--- Resto/src/main.jsx
+++ Resto/src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Joy } from './LandingPage/Components/Joy';
+import Admin from './AdminDashboard/Components/Admin';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Joy />
+    <Admin />
   </React.StrictMode>
 )
