1494149f210678f273951653d2778cfbecc4c884
janu <ignatious.janukshan20@gmail.com>
2024-06-03 23:19:20 +0530

    first commit

---
+++ Resto/src/LandingPage/Components/Joy.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../Css/Joy.css'
+
+export const Joy = () => {
+  return (
+    <div>Joy</div>
+  )
+}
---
+++ Resto/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Joy } from './LandingPage/Components/Joy';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Joy />
+  </React.StrictMode>
+)
