a8675edfc32c58cf257108e7796de57938a0abc9
Keerththan <keerththant@gmail.com>
2024-06-04 00:29:34 +0530

    reservation minor change1

--- Resto/src/main.jsx
+++ Resto/src/main.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Joy } from './LandingPage/Components/Joy';
 import Admin from './AdminDashboard/Components/Admin';
+import Reservation from './ReservationPage/Components/Reservation';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Joy />
     <Admin />
+    <Reservation/>
   </React.StrictMode>
 )
