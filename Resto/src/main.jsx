ce2d4b97b8a57a8eca8deff9f08d6419aee29242
Dev-Joyson <cj4dev@gmail.com>
2024-06-05 14:59:59 +0530

    1st commit

--- Resto/src/main.jsx
+++ Resto/src/main.jsx
@@ -1,18 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Joy } from './LandingPage/Components/Joy';
-import Admin from './AdminDashboard/Components/Admin';
-import Reservation from './ReservationPage/Components/Reservation';
-import KitchenStaff from './KitchenStaff Dashboard/Components/KitchenStaff';
-import { Customer } from './Customer Dashboard/Components/Customer';
+import { App } from './App';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Joy />
-    <Admin />
-    <Reservation/>
-    <KitchenStaff/>
-    <Customer/>
+    <App/>
   </React.StrictMode>
 )
