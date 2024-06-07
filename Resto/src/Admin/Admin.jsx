2ea27cef8c01973ec1fe5e7204415e5786f2874e
Dev-Joyson <cj4dev@gmail.com>
2024-06-07 15:55:32 +0530

    commit

--- Resto/src/Admin/Admin.jsx
+++ Resto/src/Admin/Admin.jsx
@@ -3,11 +3,8 @@ import React from 'react'
 const Admin = () => {
   return (
     <div>
-      <p>This is an admin page 2</p>
-      <p>This is an admin page 3</p>
-      
+      <p>Im the Admin</p>
     </div>
-    
   )
 }
 
