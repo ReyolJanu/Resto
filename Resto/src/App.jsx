147f147ad593a781dc9b5e51b1286f0bc0246687
Dev-Joyson <cj4dev@gmail.com>
2024-06-09 22:22:38 +0530

    cj

--- Resto/src/App.jsx
+++ Resto/src/App.jsx
@@ -1,47 +1,24 @@
 import React from 'react'
 import Admin from './Admin/Components/Admin'
-
-
 // import Routingdiv from './HostDashboard/Componenets/Routingdiv'
-
 //import Host from './HostDashboard/Componenets/Host'
 //import Reservation from './ReservationPage/Components/Reservation'
 //import KitchenStaff from './KitchenStaff Dashboard/Components/KitchenStaff'
 //import { Customer } from './Customer Dashboard/Components/Customer'
 //import NewLandingpage from './LandingPage/NewLandingpage'
 
-
 export const App = () => {
   return (
     <>
 
-
-
-
       <Admin/>
-      
       {/* <Reservation/>
-
-      {/*  */}
-      {/*
-      <Reservation/>
-
-      
-      <Customer/>
       <Host/>
       <KitchenStaff/>
-
       <Customer/>
-      <Host/>
-
+      <Routingdiv />
       <NewLandingpage /> */}
-
-      
-        {/* <NewLandingpage /> */}
-        {/* <Admin/> 
-        <Routingdiv />*/}
        
-
     </>
   )
 }
