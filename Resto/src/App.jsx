bbe8ab8fe11c4ae5d10b6cf1d9e14ae992e0595e
pirani <piranya105@gmail.com>
2024-06-11 11:11:42 +0530

    hi

--- Resto/src/App.jsx
+++ Resto/src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 //import Admin from './Admin/Components/Admin'
 //import Routingdiv from './HostDashboard/Componenets/Routingdiv'
-import Host from './HostDashboard/Componenets/Host'
+//import Host from './HostDashboard/Componenets/Host'
 //import Reservation from './ReservationPage/Components/Reservation'
 //import KitchenStaff from './KitchenStaff Dashboard/Components/KitchenStaff'
 //import { Customer } from './Customer Dashboard/Components/Customer'
-//import NewLandingpage from './LandingPage/NewLandingpage'
+import NewLandingpage from './LandingPage/NewLandingpage'
 
 export const App = () => {
   return (
@@ -17,8 +17,8 @@ export const App = () => {
         {/* <Customer/> */}
         {/* <Admin/> */}
         {/* <Routingdiv/> */} 
-        <Host/>
-        {/* <NewLandingpage />  */}
+        {/* <Host/> */}
+        <NewLandingpage /> 
 
     </>
   )
