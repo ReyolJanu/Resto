62bb8fae9a18bbea9bc548859839b8c1545f6230
janu <ignatious.janukshan20@gmail.com>
2024-06-11 12:02:53 +0530

    hi

--- Resto/src/LandingPage/CustomerRegister.jsx
+++ Resto/src/LandingPage/CustomerRegister.jsx
@@ -11,12 +11,21 @@ const CustomerRegister = ({ closeModal }) => {
 
     return (
         <div className="modal-content-customer">
-            <h2>Register as a Customer</h2>
-            <form onSubmit={handleSubmit}>
-                {/* Your form fields here */}
+            <div className="header">
+                <div className="logodiv"><img src={logo} className='logo'/></div>
+                <div className="btn-div"><button type="button" onClick={closeModal} className='close-btn'>Close</button></div>
+            </div>
+            <div className="customerform-div">
+                <form onSubmit={handleSubmit}>
+                    <div className="sec1">
+                    <input type='text' className='customer-input' placeholder='First Name'/>
+                    <input type='text' className='customer-input' placeholder='First Name'/>
+                    </div>
+
+                
                 <button type="submit">Register</button>
-                <button type="button" onClick={closeModal}>Close</button>
-            </form>
+                </form>
+            </div>
         </div>
     );
 };
