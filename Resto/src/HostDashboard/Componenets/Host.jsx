9b7bf032eb5b63a43e2af84580557bdb5656edd9
pirani <piranya105@gmail.com>
2024-06-11 10:39:43 +0530

    pire

--- Resto/src/HostDashboard/Componenets/Host.jsx
+++ Resto/src/HostDashboard/Componenets/Host.jsx
@@ -1,85 +1,110 @@
 import React from 'react'
 import '../Css/Host.css'
+import { MdSpaceDashboard } from "react-icons/md";
+import { MdGpsFixed } from "react-icons/md";
+import { MdTableRestaurant } from "react-icons/md";
+import { IoNotifications } from "react-icons/io5";
+import { MdFeedback } from "react-icons/md";
+import { IoSettingsSharp } from "react-icons/io5";
+import { IoMdLogOut } from "react-icons/io";
+import hostimage from '../images/host3.jpg';
+import logoimage from '../images/logo.png';
+
 const Host = () => {
   return (
     <div>
-      <section>
-        <div class="left">
+      
+        <div className="content">
+          <div class="left">  
+           
+           <div class="id">
+                <img  class="hostimage"src={hostimage} ></img> 
+               <span class="nav-items">Restaurant Host</span>
+            </div>
 
-          <div class="id">
-          <img src="" alt="hostimage"></img>
-          <span class="nav-items"><b>Restaurant Host</b></span>
-          </div>
-
-          <nav>  
-          <ul>
-          <li><a href="#"> 
-          <i class=""></i>
-          <span class="nav-items"><b>Dashboard</b></span>
-          </a> </li>
+           <div className="list">
+           <ul>
+           <li><a href="#"> 
+               <i class=""><MdSpaceDashboard /></i>
+               <span class="nav-items">Dashboard</span>
+           </a> </li>
           <li><a href="#">
-          <i class=""></i>
-          <span class="nav-items"><b>Reservation</b></span>
+               <i class=""><MdGpsFixed /></i>
+               <span class="nav-items">Reservation</span>
+           </a></li>
+          <li><a href="#">
+              <i class="">< MdTableRestaurant/></i> 
+              <span class="nav-items">Tables</span>
+           </a></li>
+          <li><a href="#">
+             <i class=""><IoNotifications/></i>
+             <span class="nav-items">Notification</span>
           </a></li>
           <li><a href="#">
-          <i class=""></i> 
-          <span class="nav-items"><b>Tables</b></span>
-          </a></li>
+              <i class=""><MdFeedback/></i> 
+              <span class="nav-items">Feedback</span>
+           </a></li>
           <li><a href="#">
-          <i class=""></i>
-          <span class="nav-items"><b>Notification</b></span>
-          </a></li>
-          <li><a href="#">
-          <i class=""></i> 
-          <span class="nav-items"><b>Feedback</b></span>
-          </a></li>
-          <li><a href="#">
-          <i class=""></i> 
-          <span class="nav-items"><b>Settings</b></span>
-          </a></li>
+              <i class=""><IoSettingsSharp/></i> 
+              <span class="nav-items">Settings</span>
+           </a></li>
           <li><a href="#" class="logout">
-          <i class=""></i> 
-          <span class="nav-items"><b>Logout</b></span>
-          </a></li>
-         </ul>
-         </nav>
-
-        </div>
-        <div class="top">
-           <div class="logo">
-              <img src="" alt="logoimage"></img>
+             <i class=""> <IoMdLogOut/></i> 
+             <span class="nav-items">Logout</span>
+           </a></li>
+            </ul>
            </div>
-           <div class="profile">
-              <img src="" alt="profileimage"></img>
+      
+      </div>
+
+          {/* top */}
+     <div className="right">
+     <div class="top">
+              <div class="logo">
+                 <img class="logoimage"src={logoimage} ></img> 
+              </div>
+              <div class="profile">
+                  <img  class="profileimage"src={hostimage} ></img> 
+              </div>
            </div>
-        </div>
-        <div class="main">
-          <div class="carts">
-            <p class="title">Customers</p>
-            <p class="count">30</p>
-          </div>
-          <div class="carts">
-            <p class="title">Reservation</p>
-            <p class="count">15</p>
-          </div>
-          <div class="carts">
-            <p class="title">Preorders</p>
-            <p class="count">40</p>
-          </div>
-          <div class="carts">
-            <p class="title">Notifications</p>
-            <p class="count">35</p>
-          </div>
 
-          <div class="graph">
+            {/* rooting part */}
+            <div className="rootpart">
+               <div className="cart-div">
+                    <div className="cart">
+                       <p class="title">Reservations</p>
+                       <p class="count">55</p>
+                    </div>
+                    <div className="cart">
+                       <p class="title">Preorders</p>
+                       <p class="count">60</p>
+                    </div>
+                    <div className="cart">
+                       <p class="title">Customers</p>
+                       <p class="count">45</p>
+                    </div>
+                    <div className="cart">
+                      <p class="title">Notifications</p>
+                      <p class="count">60</p>
+                    </div>
+                 </div>  
 
+                 <div className="graph-div">
+                    <div className="graph">
+                        <div className="heading"></div>
+                        <div className="image"></div>
+                     </div>
+                    <div className="graph">
+                        <div className="heading"></div>
+                        <div className="image"></div>
+                    </div>
+                 </div>
+            </div>  
+          
           </div>
-        </div>
-
-      </section>
-
-   </div>
+      </div>
+  </div>
   )
 }
 
-export default Host
+export default Host;
