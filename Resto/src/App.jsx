3d3253fc18dc90dad5f563ad55871d299a80c763
janu <ignatious.janukshan20@gmail.com>
2024-06-13 22:12:56 +0530

    naan

--- Resto/src/App.jsx
+++ Resto/src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 //import Admin from './Admin/Components/Admin'
 //import Routingdiv from './HostDashboard/Componenets/Routingdiv'
-import Host from './HostDashboard/Componenets/Host'
+//import Host from './HostDashboard/Componenets/Host'
 //import Reservation from './ReservationPage/Components/Reservation'
 //import KitchenStaff from './KitchenStaff Dashboard/Components/KitchenStaff'
 //import { Customer } from './Customer Dashboard/Components/Customer'
-//import NewLandingpage from './LandingPage/NewLandingpage'
+import NewLandingpage from './LandingPage/NewLandingpage'
 //import ModalSample from './LandingPage/ModalSample'
 
 
@@ -15,10 +15,10 @@ export const App = () => {
       {/* <Admin/> */}
       {/* <Reservation/> */}
       {/* <Customer/> */}
-      <Host/>
+      {/* <Host/> */}
       {/* <KitchenStaff/> */}
       {/* <Customer/> */}
-      {/* <NewLandingpage /> */}
+      <NewLandingpage />
     </>
   )
 }
--- Resto/src/LandingPage/CustomerRegister.jsx
+++ Resto/src/LandingPage/CustomerRegister.jsx
@@ -21,6 +21,44 @@ const CustomerRegister = ({ closeModal }) => {
                     <input type='text' className='customer-input' placeholder='First Name'/>
                     <input type='text' className='customer-input' placeholder='First Name'/>
                     </div>
+                    <div className="sec1">
+                    <input type='text' className='customer-input-address' placeholder='Postal Address'/>
+                    </div>
+                    <div className="sec1">
+                    <input type='text' className='customer-input' placeholder='City'/>
+                    <input type='text' className='customer-input-postal' placeholder='Postal Code'/>
+                    <select name="provices" className='customer-select'>
+                                <option value="Eastern">Eastern</option>
+                                <option value="Western">Western</option>
+                                <option value="Northern">Northern</option>
+                                <option value="Central">Central</option>
+                                <option value="Southern">Southern</option>
+                                <option value="North Western">North Western</option>
+                                <option value="Uva">Uva</option>
+                                <option value="Sabaragamuwa ">Sabaragamuwa </option>
+                                <option value="North Central">North Central</option>
+                            </select>
+                    </div>
+                    <div className="sec1">
+                    <input type='email' className='customer-input-address' placeholder='Email'/>
+                    </div>
+                    <div className="sec1">
+                    <input type='number' className='customer-input' placeholder='Telephone Number'/>
+                    <input type='number' className='customer-input' placeholder='LanLine Number'/>
+                    </div>
+
+                    
+                    <div className="sec1">
+                    <input type='text' className='customer-input' placeholder='NIC'/>
+                    <div className="border-div"><label className='gender-label'>Gender</label>
+                    <select name="gender" className='gender-select'> 
+                                <option value="Male">Male</option>
+                                <option value="Female">Female</option>
+                                <option value="others">Others</option>
+                            </select>    
+                    </div>  
+                    </div>
+                
 
                 
                 <button type="submit">Register</button>
