1e19dfd14ee78f17acc1a97acca206d6ef951dc4
Keerththan <keerththant@gmail.com>
2024-06-15 21:20:24 +0530

    MatirialUiUpdated

--- Resto/src/App.jsx
+++ Resto/src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 //import Admin from './Admin/Components/Admin'
 //import Routingdiv from './HostDashboard/Componenets/Routingdiv'
-import Host from './HostDashboard/Componenets/Host'
-//import Reservation from './ReservationPage/Components/Reservation'
+//import Host from './HostDashboard/Componenets/Host'
+import Reservation from './ReservationPage/Components/Reservation'
 //import KitchenStaff from './KitchenStaff Dashboard/Components/KitchenStaff'
 //import { Customer } from './Customer Dashboard/Components/Customer'
 //import NewLandingpage from './LandingPage/NewLandingpage'
@@ -13,13 +13,13 @@ export const App = () => {
   return (
     <>
 
-        {/* <Reservation/> */}
+        <Reservation/>
         {/* <Customer/> */}
         {/* <KitchenStaff/> */}
         {/* <Customer/> */}
         {/* <Admin/> */}
         {/* <Routingdiv/> */} 
-        <Host/>
+        {/* <Host/> */}
         {/* <NewLandingpage />  */}
 
     </>
