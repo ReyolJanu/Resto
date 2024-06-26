947f06e03b6c201eb5c5e974d23b54fe505e44bf
Keerththan <keerththant@gmail.com>
2024-06-26 05:35:46 +0530

    roundtable

--- Resto/src/ReservationPage/Components/Tables.jsx
+++ Resto/src/ReservationPage/Components/Tables.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 import '/src/ReservationPage/Css/tables.css';
 import tablelay from '/src/ReservationPage/Images/restaurant1.png';
 import MediumTable from './TableTypes/MediumTable';
-//import RoundTable from './TableTypes/RoundTable';
+import RoundTable from './TableTypes/RoundTable';
 //import PartyTable from './TableTypes/PartyTable';
 import CoupleTable from './TableTypes/CoupleTable';
 
  
  function Tables() {
    return (
-     <div>
+     <div class="main">
      {/* <img src={tablelay} alt="Restaurant" id="tlayout"/> */}
-    
+    <div class="col1">
      <MediumTable tablename="Table1"/>
      <hr class="short"></hr>
      <MediumTable tablename="Table2"/>
@@ -20,7 +20,22 @@ import CoupleTable from './TableTypes/CoupleTable';
      <hr class="short"></hr>
      <MediumTable tablename="Table4"/>
      <hr class="short"></hr>
+     </div>
 
+     <div class="col2">
+     <div class="vl"></div>
+     <div class="vl"></div>
+     </div>
+
+     <div class="col3">
+     <RoundTable tablename="Table1"/>
+     
+     <RoundTable tablename="Table2"/>
+     
+     <RoundTable tablename="Table3"/>
+     
+     
+     </div>
 
      </div>
    )
